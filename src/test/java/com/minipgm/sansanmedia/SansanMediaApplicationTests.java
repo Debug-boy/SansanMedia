7c44362a3f93ee43e4e556ce5bc07f3c6cea913d
Debug-girl <1936344484@qq.com>
2025-01-09 18:01:43 +0800

    新增的一个测试类，用于测试ListStrigToJsonConvert的转换是否成功

--- src/test/java/com/minipgm/sansanmedia/SansanMediaApplicationTests.java
+++ src/test/java/com/minipgm/sansanmedia/SansanMediaApplicationTests.java
@@ -1,13 +1,47 @@
 package com.minipgm.sansanmedia;
 
+import com.minipgm.sansanmedia.entity.SubscribeTask;
+import com.minipgm.sansanmedia.handler.ListStringToJsonConverter;
+import com.minipgm.sansanmedia.mapper.SubsribeTaskMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SansanMediaApplicationTests {
 
+    @Autowired
+    SubsribeTaskMapper subsribeTaskMapper;
+
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void converTest(){
+        ListStringToJsonConverter converter = new ListStringToJsonConverter();
+        String json = "[\"url1\", \"url2\", \"url3\"]";  // 这个是模拟从数据库取出的 JSON 字符串
+        try {
+            List<String> urls = converter.convertToEntityAttribute(json);
+            System.out.println(urls);  // 应该输出 [url1, url2, url3]
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testSubsribeTaskMapper2(){
+        SubscribeTask subscribeTask = subsribeTaskMapper.getTaskByWxOpenid("oLEXs6zQv_zTC65_OrYQNxXqpoXc");
+
+        if(subscribeTask != null){
+            System.out.println("success!");
+        }else{
+            System.out.println("urls list is null!");
+        }
+
+    }
+
 
 }
